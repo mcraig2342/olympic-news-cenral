5a9fe1bd5cc85e34ed8ebe857aa3c0c12a89e658
mcraig2342 <matthewcookecraig10@gmail.com>
2021-07-27 13:37:38 -0600

    Create render articles method to render cards

--- src/Components/ArticleArea/ArticleArea.js
+++ src/Components/ArticleArea/ArticleArea.js
@@ -6,11 +6,25 @@ import { React } from 'react'
 
 const ArticleArea = ({ allArticles, viewDetails }) => {
 
-
+  const renderArticles = (articles) => {
+      return articles?.map(article => {
+        return (
+          <ArticleCard
+            id={article.uri}
+            key={article.uri}
+            title={article.title}
+            image_url={article.multimedia[0].url}
+            image_caption={article.multimedia[0].caption}
+            byline={article.byline}
+            abstract={article.abstract}
+          />
+        )
+      })
+    }
 
   return (
     <div onClick={viewDetails} className='articleContainer'>
-
+        {renderArticles(allArticles)}
     </div>
   );
 }
