4b1fca9a1d3f6d68ab5770b78f6324e202b375fd
mcraig2342 <matthewcookecraig10@gmail.com>
2021-07-28 09:52:44 -0600

    Add test to make sure the aricle displayed with the correct info

---
+++ cypress/integration/landing-page-spec.js
@@ -0,0 +1,17 @@
+describe('Home page elements', () => {
+
+  beforeEach(() => {
+    cy.stub()
+    cy.visit('http://localhost:3000')
+  })
+
+
+  it('Should display article', () => {
+     cy.get('.articleCard').should('be.visible')
+       .get('.image').should('be.visible')
+       .get('.title').should('be.visible')
+       .get('p').should('be.visible')
+  });
+
+
+});
