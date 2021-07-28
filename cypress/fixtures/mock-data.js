38625d16e5096b3539a3df589ac143ad5919bf8c
mcraig2342 <matthewcookecraig10@gmail.com>
2021-07-28 09:49:10 -0600

    Create mock data

---
+++ cypress/fixtures/mock-data.js
@@ -0,0 +1,83 @@
+{"results": [
+{
+"section": "sports",
+"subsection": "olympics",
+"title": "Simone Biles Rejects a Long Tradition of Stoicism in Sports",
+"abstract": "Biles was widely embraced as the latest active, elite athlete who had the courage to acknowledge her vulnerability.",
+"url": "https://www.nytimes.com/2021/07/28/sports/olympics/simone-biles-mental-health.html",
+"uri": "nyt://article/471b244f-b3d4-5569-8746-0a137263b10e",
+"byline": "By Jeré Longman",
+"item_type": "Article",
+"updated_date": "2021-07-28T09:59:08-04:00",
+"created_date": "2021-07-28T05:00:51-04:00",
+"published_date": "2021-07-28T05:00:51-04:00",
+"material_type_facet": "",
+"kicker": "",
+"des_facet": [
+"Olympic Games (2020)",
+"Gymnastics",
+"Mental Health and Disorders"
+],
+"org_facet": [],
+"per_facet": [
+"Biles, Simone",
+"Burrell, Leroy",
+"Phelps, Michael",
+"Osaka, Naomi (1997- )"
+],
+"geo_facet": [],
+"multimedia": [
+{
+"url": "https://static01.nyt.com/images/2021/07/28/sports/28olympics-pressure-1/merlin_191704776_d68d8adb-0796-4341-a3d7-0dd1f07fda3a-superJumbo.jpg",
+"format": "superJumbo",
+"height": 1365,
+"width": 2048,
+"type": "image",
+"subtype": "photo",
+"caption": "Simone Biles withdrew from the team gymnastics event and then said she would skip the all-around, too. ",
+"copyright": "Doug Mills/The New York Times"
+},
+{
+"url": "https://static01.nyt.com/images/2021/07/28/sports/28olympics-pressure-1/merlin_191704776_d68d8adb-0796-4341-a3d7-0dd1f07fda3a-thumbStandard.jpg",
+"format": "Standard Thumbnail",
+"height": 75,
+"width": 75,
+"type": "image",
+"subtype": "photo",
+"caption": "Simone Biles withdrew from the team gymnastics event and then said she would skip the all-around, too. ",
+"copyright": "Doug Mills/The New York Times"
+},
+{
+"url": "https://static01.nyt.com/images/2021/07/28/sports/28olympics-pressure-1/merlin_191704776_d68d8adb-0796-4341-a3d7-0dd1f07fda3a-thumbLarge.jpg",
+"format": "thumbLarge",
+"height": 150,
+"width": 150,
+"type": "image",
+"subtype": "photo",
+"caption": "Simone Biles withdrew from the team gymnastics event and then said she would skip the all-around, too. ",
+"copyright": "Doug Mills/The New York Times"
+},
+{
+"url": "https://static01.nyt.com/images/2021/07/28/sports/28olympics-pressure-1/merlin_191704776_d68d8adb-0796-4341-a3d7-0dd1f07fda3a-mediumThreeByTwo210.jpg",
+"format": "mediumThreeByTwo210",
+"height": 140,
+"width": 210,
+"type": "image",
+"subtype": "photo",
+"caption": "Simone Biles withdrew from the team gymnastics event and then said she would skip the all-around, too. ",
+"copyright": "Doug Mills/The New York Times"
+},
+{
+"url": "https://static01.nyt.com/images/2021/07/28/sports/28olympics-pressure-1/merlin_191704776_d68d8adb-0796-4341-a3d7-0dd1f07fda3a-articleInline.jpg",
+"format": "Normal",
+"height": 127,
+"width": 190,
+"type": "image",
+"subtype": "photo",
+"caption": "Simone Biles withdrew from the team gymnastics event and then said she would skip the all-around, too. ",
+"copyright": "Doug Mills/The New York Times"
+}
+],
+"short_url": "https://nyti.ms/3ynBCgZ"
+}]
+}
