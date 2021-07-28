15966f24c6eeceac6ff20369311dc80a68fed4ff
mcraig2342 <matthewcookecraig10@gmail.com>
2021-07-27 20:32:21 -0600

    Added the image to the link

--- src/Components/ArticleCard/ArticleCard.js
+++ src/Components/ArticleCard/ArticleCard.js
@@ -5,11 +5,11 @@ import './ArticleCard.css'
 const ArticleCard = ({ id, title, image_url, image_caption, byline, abstract }) => {
 
     return (
-      <article id={id} key={id} className='article-card'>
-      <img id={id} className='image' src={image_url} alt={image_caption}/>
-      <Link className='link' to={`/${id}`}>
-          <h1 id={id}>{title}</h1>
-      </Link>
+      <article id={id} key={id} className='articleCard'>
+       <Link className='link' to={`/${id}`}>
+          <img id={id} className='image' src={image_url} alt={image_caption}/>
+          <h1 className='title' id={id}>{title}</h1>
+       </Link>
           <p id={id}>{byline}</p>
       </article>
     );
