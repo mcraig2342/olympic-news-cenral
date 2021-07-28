15e2efac3675ab54a829e8fd9ed6ff08693f22f9
mcraig2342 <matthewcookecraig10@gmail.com>
2021-07-27 23:18:49 -0600

    change link to nav link and add inlide styling to remove text decoration

--- src/Components/ArticleCard/ArticleCard.js
+++ src/Components/ArticleCard/ArticleCard.js
@@ -1,15 +1,15 @@
 import { React } from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './ArticleCard.css'
 
 const ArticleCard = ({ id, title, image_url, image_caption, byline, abstract }) => {
 
     return (
       <article id={id} key={id} className='articleCard'>
-       <Link className='link' to={`/${id}`}>
+       <NavLink style={{ textDecoration: 'none' }} className='link' to={`/${id}`}>
           <img id={id} className='image' src={image_url} alt={image_caption}/>
           <h1 className='title' id={id}>{title}</h1>
-       </Link>
+       </NavLink>
           <p id={id}>{byline}</p>
       </article>
     );
